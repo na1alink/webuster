658e4e427138782a4c27c95d99b9d0db50a89b54
na1alink <na1alinl@yandex.ru>
2024-12-09 15:42:04 +0500

    feat: added modals

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -77,3 +77,48 @@ document.querySelector(".menu-overlay").addEventListener("click", function (e) {
 
 const im = new Inputmask({ mask: "+7 (9 9 9) 9 9 9 9 9-9 9" });
 im.mask(document.querySelectorAll(".phonemask"));
+
+class Modal {
+  constructor(id) {
+    this.id = id;
+    this.window = document.getElementById(id);
+    this.modal = this.window.querySelector(".modal");
+    this.closeBtn = this.window.querySelector(".modal-close");
+    this.form = this.window.querySelector("FORM");
+  }
+
+  open() {
+    this.window.classList.add("opened");
+    this.window.addEventListener("click", (e) => this._handleClick(e));
+    this.closeBtn?.addEventListener("click", () => this.close());
+    this.form?.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+    document.querySelector(".wrap-modals-all").classList.add("opened");
+  }
+
+  close() {
+    this.window.removeEventListener("click", (e) => this._handleClick(e));
+    this.closeBtn?.removeEventListener("click", () => this.close());
+    this.window.classList.remove("opened");
+    document.querySelector(".wrap-modals-all").classList.remove("opened");
+  }
+
+  _handleClick(e) {
+    if (e.target === this.window || e.target.matches(".modal-link")) {
+      this.close();
+    }
+  }
+}
+
+(function () {
+  const modalButtons = document.querySelectorAll("[data-modal-id]");
+
+  modalButtons.forEach((btn) => {
+    if (btn) {
+      const id = btn.dataset.modalId;
+      const modal = new Modal(id);
+      btn.addEventListener("click", () => modal.open());
+    }
+  });
+})();
